3d760103fb68c0bd8d7499f54922b74896d6c233
authzyonxz <authzyonxz@users.noreply.github.com>
2026-05-24 17:20:14 +0000

    Remover aba Gerar Key Public do design novo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -32,7 +32,7 @@ import AllHackUpdateIp from "@/pages/AllHackUpdateIp";
 import FreeFireUpdateIp from "@/pages/FreeFireUpdateIp";
 import ChukyXiterUpdateIp from "@/pages/ChukyXiterUpdateIp";
 import ProxyGranjeiro from "@/pages/ProxyGranjeiro";
-import GerarKeyPublic from "@/pages/GerarKeyPublic";
+
 import JzXiterUpdateIp from "@/pages/JzXiterUpdateIp";
 
 function Router() {
@@ -51,7 +51,7 @@ function Router() {
       <Route path="/free-fire-update-ip" component={FreeFireUpdateIp} />
       <Route path="/chuky-xiter-update-ip" component={ChukyXiterUpdateIp} />
       <Route path="/proxy-granjeiro" component={ProxyGranjeiro} />
-      <Route path="/gerar-key-public" component={GerarKeyPublic} />
+
       <Route path="/jz-xiter-update-ip" component={JzXiterUpdateIp} />
       <Route path="/dashboard">
         <ProtectedRoute>
